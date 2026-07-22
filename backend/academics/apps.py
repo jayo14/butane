fc5042479f82a650703baa84cb8723adada110cc
jayo14 <ayobamidayo2007@gmail.com>
2026-07-22 09:19:16 +0100

    Import signals in AcademicsConfig.ready()

--- backend/academics/apps.py
+++ backend/academics/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AcademicsConfig(AppConfig):
     default_auto_field = "django.db.models.UUIDField"
     name = "academics"
+
+    def ready(self):
+        import academics.signals  # noqa: F401
